fbad41ba8b687c47e6943b5162c74091de32f4be
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2019-11-25 17:47:02 -0800

    Implemented selector UI to show player which program is selected

---
+++ Cryptopunk/Assets/UI/SelectedIndicator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedIndicator : MonoBehaviour
+{
+    private SpriteRenderer selectionIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectionIndicator = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Program.selectedProgram&&Program.selectedProgram.myTile)
+        {
+            selectionIndicator.enabled = true;
+            gameObject.transform.position = Program.selectedProgram.transform.position;
+        }
+        else
+        {
+            selectionIndicator.enabled = false;
+        }
+    }
+}
