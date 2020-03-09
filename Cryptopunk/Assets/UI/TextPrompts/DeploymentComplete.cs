4430f9154a33fba8c76ba61c87fe1517fc761078
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-03-09 14:05:51 -0700

    Implemented text prompt system to teach new players how to play

---
+++ Cryptopunk/Assets/UI/TextPrompts/DeploymentComplete.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeploymentComplete : MonoBehaviour
+{
+    [SerializeField] GameObject[] promptUIObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleAll(false);
+    }
+
+    private void ToggleAll(bool isOn)
+    {
+        foreach (GameObject @object in promptUIObjects)
+        {
+            Image image = @object.GetComponent<Image>();
+            Text text = @object.GetComponent<Text>();
+            Button button = @object.GetComponent<Button>();
+            if (image)
+            {
+                image.enabled = isOn;
+            }
+            if (text)
+            {
+                text.enabled = isOn;
+            }
+            if (button)
+            {
+                button.enabled = isOn;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDeploymentComplete())
+        {
+            ToggleAll(true);
+        }
+    }
+
+    internal static bool IsDeploymentComplete()
+    {
+        bool isDeploymentComplete = true;
+        foreach (Program program in DungeonManager.instance.GetPlayerControlledPrograms())
+        {
+            isDeploymentComplete &= program.myTile;
+        }
+
+        return isDeploymentComplete;
+    }
+}
---
+++ Cryptopunk/Assets/UI/TextPrompts/TextPromptManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPromptManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] textPromptObjects;
+    private List<TextPrompt> textPrompts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textPrompts = new List<TextPrompt>();
+        foreach(GameObject @object in textPromptObjects)
+        {
+            textPrompts.Add(@object.GetComponent<TextPrompt>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextPrompt toDisplay = null;
+        if (DeploymentComplete.IsDeploymentComplete())
+        {
+            foreach (Program program in DungeonManager.instance.GetPlayerControlledPrograms())
+            {
+                foreach (TextPrompt prompt in textPrompts)
+                {
+                    foreach (Vector3Int triggerPoint in prompt.TriggerPoints)
+                    {
+                        if (triggerPoint.x == program.myTile.xCoord && triggerPoint.z == program.myTile.zCoord)
+                        {
+                            toDisplay = prompt;
+                        }
+                    }
+                }
+            }
+        }
+        if(toDisplay)
+        {
+            Instantiate(toDisplay, FindObjectOfType<Canvas>().transform);
+            textPrompts.Remove(toDisplay);
+        }
+    }
+}
