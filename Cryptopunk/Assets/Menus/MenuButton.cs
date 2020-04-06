1f510f73b4f96a7fe6374cae72ad399a7c752cce
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-04-06 16:57:55 -0700

    Added feedback for mousing over menu items Wired up back button on load game screen

---
+++ Cryptopunk/Assets/Menus/MenuButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+{
+    internal static MenuButton selectedOption;
+
+    [SerializeField] AudioClip selectSound;
+    [SerializeField] AudioClip mouseOverSound;
+    [SerializeField] bool isInitialSelection;
+
+    private void Start()
+    {
+        if(isInitialSelection)
+        {
+            selectedOption = this;
+            FindObjectOfType<MenuSelectionIndicator>().Initialize();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioSource.PlayClipAtPoint(selectSound, Vector3.zero);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (this != selectedOption)
+        {
+            selectedOption = this;
+            AudioSource.PlayClipAtPoint(mouseOverSound, Vector3.zero);
+        }
+    }
+}
---
+++ Cryptopunk/Assets/Menus/MenuSelectionIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelectionIndicator : MonoBehaviour
+{
+    private Vector3 menuOffset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = MenuButton.selectedOption.transform.position + menuOffset;
+    }
+
+    internal void Initialize()
+    {
+        menuOffset = gameObject.transform.position - MenuButton.selectedOption.transform.position;
+    }
+}
