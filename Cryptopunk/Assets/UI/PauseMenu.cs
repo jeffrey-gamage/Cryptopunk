dc3ceba1eb654e9cf8adde4d37c8fc81dbf3cd11
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-04-29 17:26:35 -0700

    Created pause panel and added to level canvases

---
+++ Cryptopunk/Assets/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleActivation();
+        }
+    }
+    public void AbortMission()
+    {
+        Destroy(MissionStatus.instance.gameObject);
+        SceneManager.LoadScene("desktop");
+    }
+
+    public void ToggleActivation()
+    {
+        bool newEnabledStatus = !GetComponent<Image>().enabled;
+        foreach(Image image in GetComponentsInChildren<Image>())
+        {
+            image.enabled = newEnabledStatus;
+        }
+        foreach (Text text in GetComponentsInChildren<Text>())
+        {
+            text.enabled = newEnabledStatus;
+        }
+        foreach (Button button in GetComponentsInChildren<Button>())
+        {
+            button.enabled = newEnabledStatus;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
