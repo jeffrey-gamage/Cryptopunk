4aa7228f38e7d45efcaa1d1c1af83708c52fd92d
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-05-19 17:59:54 -0700

    Implemented search and destroy turn indicator

--- Cryptopunk/Assets/UI/SearchAndDestroy.cs
+++ Cryptopunk/Assets/UI/SearchAndDestroy.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SearchAndDestroy : MonoBehaviour
 {
+    [SerializeField] float minOpacity = 0.3f;
+    [SerializeField] float opacityDelta = 0.5f;
+    UnityEngine.UI.Text enemyActivityWarning;
+    bool flashUp = false;
+    float opacity = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyActivityWarning = GetComponent<UnityEngine.UI.Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(DungeonManager.instance.isPlayerTurn)
+        {
+            opacity = Mathf.Max(0f, opacity - Time.deltaTime * opacityDelta);
+        }
+        else
+        {
+            if (flashUp)
+            {
+                opacity = Mathf.Min(1f, opacity + Time.deltaTime * opacityDelta);
+                flashUp = !(opacity >= 1f);
+            }
+            else
+            {
+                opacity = Mathf.Max(minOpacity, opacity - Time.deltaTime * opacityDelta);
+                flashUp = opacity <= minOpacity;
+            }
+        }
+        enemyActivityWarning.color = new Color(enemyActivityWarning.color.r, enemyActivityWarning.color.g, enemyActivityWarning.color.b, opacity);
     }
 }
